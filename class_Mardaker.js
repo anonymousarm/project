1055b6cf9e650ccc9f21a958e077a1dd64f0a833
yourusername <your@email.com>
2018-12-11 19:19:50 +0400

    liq@ banem poxe

---
+++ class_Mardaker.js
@@ -0,0 +1,112 @@
+
+module.exports = class Mardaker extends LivingCreature {
+    constructor(x, y,){
+        super(x, y,);
+        this.energy = 5;
+        [
+            [this.x - 2, this.y - 2],
+            [this.x, this.y - 2],
+            [this.x + 2, this.y - 2],
+            [this.x - 2, this.y],
+            [this.x + 2, this.y],
+            [this.x - 2, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x + 2, this.y + 2]
+        ];
+    }
+    getNewDirections() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ]
+        [
+            [this.x - 2, this.y - 2],
+            [this.x, this.y - 2],
+            [this.x + 2, this.y - 2],
+            [this.x - 2, this.y],
+            [this.x + 2, this.y],
+            [this.x - 2, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x + 2, this.y + 2]
+        ]
+    }
+
+    chooseCell(character) {
+        this.getNewDirections()
+        var found = []
+        for (var i in this.directions) {
+            var x = this.directions[i][0]
+            var y = this.directions[i][1]
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i])
+                }
+            }
+
+        }
+        return found;
+
+    }
+
+    mult() {
+        var empty = random(this.chooseCell(0))
+        if (empty && this.energy > 10) {
+            var newX = empty[0]
+            var newY = empty[1]
+            matrix[newY][newX] = 1
+            var mk = new Mardaker(newX, newY)
+            mardakerArr.push(mk)
+        }
+    }
+
+    move() {
+        var empty = random(this.chooseCell(0))
+        this.energy--;
+        if (empty) {
+            var newX = empty[0]
+            var newY = empty[1]
+            matrix[newY][newX] = 5
+            matrix[this.y][this.x] = 0
+
+            this.x = newX
+            this.y = newY
+        }
+    }
+    eat() {
+        var food = random(this.chooseCell(4))
+        if (food) {
+            var newX = food[0]
+            var newY = food[1]
+            matrix[newY][newX] = 5
+            matrix[this.y][this.x] = 0
+
+            for (var i in mardArr) {
+                if (mardArr[i].x == newX && mardArr[i].y == newY) {
+                    mardArr.splice(i, 1)
+                }
+            }
+
+            this.x = newX
+            this.y = newY
+            this.energy += 5
+        }
+    }
+
+    die() {
+        if (this.energy <= 0) {
+            matrix[this.y][this.x] = 0
+            for (var i in mardakerArr) {
+                if (mardakerArr[i].x == this.x && mardakerArr[i].y == this.y) {
+                    mardakerArr.splice(i, 1)
+                }
+            }
+        }
+    }
+}
+
